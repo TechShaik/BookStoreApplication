7b06b7e19d6747a2a94b7fcc08e5f60a0a2aa1c8
shaiksha <shaiknaveed5811@gmail.com>
2024-12-06 17:45:49 +0530

    Book Store Application

---
+++ User/src/main/java/com/Files/UserEntity/Config/JwtAuthenticationFilter.java
@@ -0,0 +1,57 @@
+package com.Files.UserEntity.Config;
+
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.Files.UserEntity.Service.JwtService;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+	@Autowired
+	private JwtService jwtService;
+
+	@Autowired
+	private UserDetailsService detailsService;
+
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+			throws ServletException, IOException {
+
+		String authHeader = request.getHeader("Authorization");
+		if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+			filterChain.doFilter(request, response);
+			return;
+		}
+
+		String token = authHeader.substring(7);
+		final String username = jwtService.extractUsername(token);
+
+		Authentication existingAuth = SecurityContextHolder.getContext().getAuthentication();
+		if (username != null && existingAuth == null) {
+			UserDetails userDetails = detailsService.loadUserByUsername(username);
+			if (jwtService.isTokenValid(token, userDetails)) {
+				UsernamePasswordAuthenticationToken authenticationToken =
+						new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+				authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+				SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+			}
+		}
+
+		filterChain.doFilter(request, response);
+	}
+}
---
+++ User/src/main/java/com/Files/UserEntity/CustomClasses/CustomUserDetails.java
@@ -0,0 +1,59 @@
+package com.Files.UserEntity.CustomClasses;
+
+import java.util.Collection;
+import java.util.Collections;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.Files.UserEntity.User;
+
+import lombok.ToString;
+
+@ToString
+public class CustomUserDetails implements UserDetails {
+
+	private String username;
+	private String password;
+
+	public CustomUserDetails(User user) {
+		this.username = user.getUsername();
+		this.password = user.getUser_password();
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return Collections.singleton(new SimpleGrantedAuthority("USER"));
+	}
+
+	@Override
+	public String getPassword() {
+		return password;
+	}
+
+	@Override
+	public String getUsername() {
+		return username;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
+}
